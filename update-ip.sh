10067c09fe91d37f60134d056a70d5b60ecb3579
Eunhak Lee <lee@enak.kr>
2023-03-01 02:11:20 +0900

    Implement re-run & sleep interval

--- update-ip.sh
+++ update-ip.sh
@@ -3,8 +3,26 @@ if [[ "${TOKEN}" == "" ]] || [[ "${DOMAIN}" == "" ]] || [[ "${ZONE_ID}" == "" ]]
 	exit 1
 fi
 
-EXTERNAL_IP=$(curl -s https://ipipip.kr/raw)
+run_count=0
 
+if [ ! -z $INTERVAL ]; then
+  continuous=true
+else
+  continuous=false
+fi
+
+while [[ "$run_count" == "0" ]] || [[ $continuous = true ]]; do
+
+if [[ "$run_count" == "0" ]]; then
+  run_count=$((run_count + 1));
+elif [[ $continuous = true ]]; then
+  echo "Sleeping ${INTERVAL} before next run"
+  sleep $INTERVAL
+  echo
+  echo
+fi
+
+EXTERNAL_IP=$(curl -s https://ipipip.kr/raw)
 echo Your IP is: $EXTERNAL_IP
 
 cloudflare_record_info=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?type=A&name=$DOMAIN" \
@@ -18,12 +36,14 @@ dns_record_ip=$(echo ${cloudflare_record_info} | grep -o '"content":"[^"]*' | cu
 if [[ "${cloudflare_dns_record_id}" == "" ]]; then
 	echo Error on fetching DNS Record ID
 	echo $cloudflare_record_info
-	exit 1
+
+  if [[ $continuous = false ]]; then exit 1; else continue; fi
 fi
 
 if [[ "${dns_record_ip}" == "${EXTERNAL_IP}" ]]; then
 	echo "DNS IP(${dns_record_ip}) is identical with External IP(${EXTERNAL_IP})"
-	exit 0
+
+	if [[ $continuous = false ]]; then exit 0; else continue; fi
 fi
 
 update_dns_record=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$cloudflare_dns_record_id" \
@@ -34,8 +54,10 @@ update_dns_record=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$
 if [[ "${update_dns_record}" == *"\"success\":false"* ]]; then
   echo ${update_dns_record}
   echo "Error! Update Failed"
-  exit 0
+  if [[ $continuous = true ]]; then exit 0 else continue; fi
 fi
 
 echo "==> Success!"
 echo "==> $DOMAIN DNS Record Updated To: $EXTERNAL_IP, ttl: 120, proxied: $is_proxied"
+
+done;
