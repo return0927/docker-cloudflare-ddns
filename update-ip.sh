b9c9d70b2aa952df6797b61a84b27a09562b189e
Eunhak Lee <lee@enak.kr>
2023-02-28 23:55:32 +0900

    Match if statement syntax to alpine linux

--- update-ip.sh
+++ update-ip.sh
@@ -1,3 +1,8 @@
+if [[ "${TOKEN}" == "" ]] || [[ "${DOMAIN}" == "" ]] || [[ "${ZONE_ID}" == "" ]]; then
+	echo Required variable is not set
+	exit 1
+fi
+
 EXTERNAL_IP=$(curl -s https://ipipip.kr/raw)
 
 echo Your IP is: $EXTERNAL_IP
@@ -10,13 +15,13 @@ cloudflare_dns_record_id=$(echo ${cloudflare_record_info} | grep -o '"id":"[^"]*
 is_proxied=$(echo ${cloudflare_record_info} | grep -o '"proxied":[^,]*' | grep -o '[^:]*$')
 dns_record_ip=$(echo ${cloudflare_record_info} | grep -o '"content":"[^"]*' | cut -d'"' -f 4)
 
-if [[ ${cloudflare_dns_record_id} == "" ]]; then
+if [[ "${cloudflare_dns_record_id}" == "" ]]; then
 	echo Error on fetching DNS Record ID
 	echo $cloudflare_record_info
 	exit 1
 fi
 
-if [[ ${dns_record_ip} == ${EXTERNAL_IP} ]]; then
+if [[ "${dns_record_ip}" == "${EXTERNAL_IP}" ]]; then
 	echo "DNS IP(${dns_record_ip}) is identical with External IP(${EXTERNAL_IP})"
 	exit 0
 fi
@@ -26,7 +31,7 @@ update_dns_record=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$
     -H "Content-Type: application/json" \
     --data "{\"type\":\"A\",\"name\":\"$DOMAIN\",\"content\":\"$EXTERNAL_IP\",\"ttl\":120,\"proxied\":$is_proxied}")
 
-if [[ ${update_dns_record} == *"\"success\":false"* ]]; then
+if [[ "${update_dns_record}" == *"\"success\":false"* ]]; then
   echo ${update_dns_record}
   echo "Error! Update Failed"
   exit 0
